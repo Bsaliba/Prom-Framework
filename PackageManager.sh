e56f2afaa7182038b1ed7b6229202d0519e91c27
hverbeek <hverbeek@a68f48d4-8d00-424f-b812-3f22a534a02a>
2014-03-10 09:02:40 +0000

    Added Linux shell scripts for nightly build. Copied and renamed them from release 6.3.

---
+++ PackageManager.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+###
+## ProM specific
+###
+PROGRAM=ProM63
+CP=./${PROGRAM}.jar
+LIBDIR=./lib
+MAIN=org.processmining.contexts.uitopia.packagemanager.PMFrame
+
+####
+## Environment options
+###
+JAVA=/usr/java/default/bin/java
+MEM=1g
+
+###
+## Main program
+###
+
+add() {
+	CP=${CP}:$1
+}
+
+
+for lib in $LIBDIR/*.jar
+do
+	add $lib
+done
+
+$JAVA -classpath ${CP} -Djava.library.path=${LIBDIR} -ea -Xmx${MEM} -XX:+UseCompressedOops ${MAIN}
