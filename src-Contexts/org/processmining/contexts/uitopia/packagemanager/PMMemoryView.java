c90a27288b5c8be7636ac72178c9b9e5fe68404e
hverbeek <hverbeek@a68f48d4-8d00-424f-b812-3f22a534a02a>
2014-10-13 12:31:54 +0000

    Fixes #3049. Allowed more memory options.

--- src-Contexts/org/processmining/contexts/uitopia/packagemanager/PMMemoryView.java
+++ src-Contexts/org/processmining/contexts/uitopia/packagemanager/PMMemoryView.java
@@ -11,6 +11,7 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -21,61 +22,150 @@ import org.processmining.framework.util.OsUtil;
 
 import com.fluxicon.slickerbox.components.RoundedPanel;
 
-public class PMMemoryView extends RoundedPanel {
+public class PMMemoryView extends RoundedPanel implements ActionListener {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2483996283422006055L;
-	
+
+	/*
+	 * Get whether we are running in 64 bit mode, this allows allocating more
+	 * than 1300 MB of memory.
+	 */
 	private static boolean is64bit = OsUtil.is64Bit();
+	/*
+	 * Get the available memory.
+	 */
 	private static long mem = OsUtil.getPhysicalMemory() / (1024 * 1024);
-	
-	private final static String XMX1G = "1G";
-	private final static String XMX1300M = "1300M";
-	private final static String XMX2G = "2G";
-	private final static String XMX4G = "4G";
-	private final static String XMX8G = "8G";
-	private final static String XMX16G = "16G";
 
-	private static String selectedMem = XMX1G;
-	private static String oldSelectedMem = XMX1G;
+	/*
+	 * Start with using 1 GB of memory.
+	 */
+	private static MemoryOption selectedMem = MemoryOption.XMX1G;
+	private static MemoryOption oldSelectedMem = MemoryOption.XMX1G;
 
-	private ImageLozengeButton button1gbSelected;
-	private ImageLozengeButton button1300mbSelected;
-	private ImageLozengeButton button2gbSelected;
-	private ImageLozengeButton button4gbSelected;
-	private ImageLozengeButton button8gbSelected;
-	private ImageLozengeButton button16gbSelected;
-	private ImageLozengeButton button1gbNotSelected;
-	private ImageLozengeButton button1300mbNotSelected;
-	private ImageLozengeButton button2gbNotSelected;
-	private ImageLozengeButton button4gbNotSelected;
-	private ImageLozengeButton button8gbNotSelected;
-	private ImageLozengeButton button16gbNotSelected;
-	
+	private enum MemoryOption implements ActionListener {
+		XMX1G("1G", true), // 1 GB
+		XMX1300M("1300M", true), // 1300 MB, about the limit in 32 bit mode
+		XMX2G("2G", is64bit && mem >= 2 * 1024), // 2 GB
+		XMX3G("3G", is64bit && mem >= 3 * 1024), // 3 GB
+		XMX4G("4G", is64bit && mem >= 4 * 1024), // 4 GB
+		XMX6G("6G", is64bit && mem >= 6 * 1024), // 6 GB
+		XMX8G("8G", is64bit && mem >= 8 * 1024), // 8 GB
+		XMX12G("12G", is64bit && mem >= 12 * 1024), // 12 GB
+		XMX16G("16G", is64bit && mem >= 16 * 1024); // 16 GB
+
+		/*
+		 * Text to be added to "-Xmx"
+		 */
+		private String size;
+		/*
+		 * Whether this memory option is available on this computer.
+		 */
+		private boolean isAvailable;
+		/*
+		 * Button to show when this option is selected.
+		 */
+		private ImageLozengeButton selected;
+		/*
+		 * Button to show when this option is not selected.
+		 */
+		private ImageLozengeButton notSelected;
+
+		/*
+		 * Creates a memory option.
+		 */
+		private MemoryOption(String size, boolean isAllowed) {
+			this.size = size;
+			this.isAvailable = isAllowed;
+		}
+
+		/*
+		 * Returns the string to add to "-Xmx".
+		 */
+		public String toString() {
+			return size;
+		}
+
+		/*
+		 * Returns the string to put on the buttons.
+		 */
+		public String toDisplay() {
+			return size.replace("G", " GB").replace("M", " MB");
+		}
+
+		/*
+		 * Returns whether this option is available.
+		 */
+		public boolean isAvailable() {
+			return isAvailable;
+		}
+
+		/*
+		 * Returns the current shown button.
+		 */
+		public JButton getButton(boolean isSelected) {
+			return isSelected ? selected : notSelected;
+		}
+
+		/*
+		 * Initialize this memory option. Creates the buttons, sets the tool
+		 * tip, and the action listener.
+		 */
+		public void init(ActionListener actionListener) {
+			selected = new ImageLozengeButton(ImageLoader.load("remove_30x30_black.png"), toDisplay());
+			selected.setEnabled(false);
+			notSelected = new ImageLozengeButton(ImageLoader.load("action_30x30_black.png"), toDisplay());
+			notSelected.setToolTipText("Use " + toDisplay() + " of memory when running ProM");
+			notSelected.addActionListener(actionListener);
+		}
+
+		/*
+		 * Act on a button being selected.
+		 * 
+		 * @see
+		 * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent
+		 * )
+		 */
+		public void actionPerformed(ActionEvent e) {
+			/*
+			 * Check whether my button was selected.
+			 */
+			if (e.getSource() == notSelected) {
+				/*
+				 * Yes, it was. Set my memory size.
+				 */
+				selectedMem = this;
+			}
+		}
+	};
+
+	/*
+	 * Creates the memory view panel.
+	 */
 	public PMMemoryView() {
 		super(20, 5, 0);
 		setBackground(new Color(160, 160, 160));
 		setLayout(new BorderLayout());
 		try {
+			/*
+			 * Try to read the current memory option from file, and set the
+			 * default accordingly.
+			 */
 			FileReader reader = new FileReader("ProM641.l4j.ini");
 			char[] a = new char[10];
 			reader.read(a);
 			reader.close();
 			String b = String.valueOf(a);
-			if (b.startsWith("-Xmx1G")) {
-				selectedMem = XMX1G;
-			} else if (b.startsWith("-Xmx1300M")) {
-				selectedMem = XMX1300M; // About as much as a 32-bit VM can handle.
-			} else if (b.startsWith("-Xmx2G")) {
-				selectedMem = XMX2G;
-			} else if (b.startsWith("-Xmx4G")) {
-				selectedMem = XMX4G;
-			} else if (b.startsWith("-Xmx8G")) {
-				selectedMem = XMX8G;
-			} else if (b.startsWith("-Xmx16G")) {
-				selectedMem = XMX16G;
+			for (MemoryOption size : MemoryOption.values()) {
+				if (b.startsWith("-Xmx" + size)) {
+					/*
+					 * Found a match. Set this option as default.
+					 */
+					selectedMem = size;
+					continue;
+				}
 			}
 		} catch (FileNotFoundException e) {
 		} catch (IOException e) {
@@ -83,116 +173,104 @@ public class PMMemoryView extends RoundedPanel {
 		setupUI();
 		update();
 	}
-	
+
+	/*
+	 * Sets up the buttons.
+	 */
 	private void setupUI() {
-		button1gbSelected = new ImageLozengeButton(ImageLoader.load("remove_30x30_black.png"), "1 GB");
-		button1300mbSelected = new ImageLozengeButton(ImageLoader.load("remove_30x30_black.png"), "1300 MB");
-		button2gbSelected = new ImageLozengeButton(ImageLoader.load("remove_30x30_black.png"), "2 GB");
-		button4gbSelected = new ImageLozengeButton(ImageLoader.load("remove_30x30_black.png"), "4 GB");
-		button8gbSelected = new ImageLozengeButton(ImageLoader.load("remove_30x30_black.png"), "8 GB");
-		button16gbSelected = new ImageLozengeButton(ImageLoader.load("remove_30x30_black.png"), "16 GB");
-		button1gbSelected.setEnabled(false);
-		button1300mbSelected.setEnabled(false);
-		button2gbSelected.setEnabled(false);
-		button4gbSelected.setEnabled(false);
-		button8gbSelected.setEnabled(false);
-		button16gbSelected.setEnabled(false);
-		button1gbNotSelected = new ImageLozengeButton(ImageLoader.load("action_30x30_black.png"), "1 GB");
-		button1300mbNotSelected = new ImageLozengeButton(ImageLoader.load("action_30x30_black.png"), "1300 MB");
-		button2gbNotSelected = new ImageLozengeButton(ImageLoader.load("action_30x30_black.png"), "2 GB");
-		button4gbNotSelected = new ImageLozengeButton(ImageLoader.load("action_30x30_black.png"), "4 GB");
-		button8gbNotSelected = new ImageLozengeButton(ImageLoader.load("action_30x30_black.png"), "8 GB");
-		button16gbNotSelected = new ImageLozengeButton(ImageLoader.load("action_30x30_black.png"), "16 GB");
-		button1gbNotSelected.setToolTipText("Use 1 GB of memory when running ProM");
-		button1gbNotSelected.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				selectedMem = XMX1G;
-				update();
-			}
-		});
-		button1300mbNotSelected.setToolTipText("Use 1300 MB of memory (limit on 32-bit VMs) when running ProM");
-		button1300mbNotSelected.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				selectedMem = XMX1300M;
-				update();
-			}
-		});
-		button2gbNotSelected.setToolTipText("Use 2 GB of memory when running ProM");
-		button2gbNotSelected.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				selectedMem = XMX2G;
-				update();
-			}
-		});
-		button4gbNotSelected.setToolTipText("Use 4 GB of memory when running ProM");
-		button4gbNotSelected.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				selectedMem = XMX4G;
-				update();
-			}
-		});
-		button8gbNotSelected.setToolTipText("Use 8 GB of memory when running ProM");
-		button8gbNotSelected.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				selectedMem = XMX8G;
-				update();
-			}
-		});
-		button16gbNotSelected.setToolTipText("Use 16 GB of memory when running ProM");
-		button16gbNotSelected.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				selectedMem = XMX16G;
-				update();
-			}
-		});
-		
+		/*
+		 * Initialize all buttons.
+		 */
+		for (MemoryOption size : MemoryOption.values()) {
+			size.init(this);
+		}
 	}
 
+	/*
+	 * A button was pressed. Check every option, and then update.
+	 */
+	public void actionPerformed(ActionEvent e) {
+		for (MemoryOption size : MemoryOption.values()) {
+			/*
+			 * Check whether the button belongs to this option.
+			 */
+			size.actionPerformed(e);
+		}
+		update();
+	}
+
+	/*
+	 * Update the memory view.
+	 */
 	private void update() {
 		updateFiles();
+		/*
+		 * Create new buttonPanel.
+		 */
 		JPanel buttonPanel = new RoundedPanel(20, 5, 0);
 		buttonPanel.setBackground(new Color(80, 80, 80));
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
-		buttonPanel.add(selectedMem.equals(XMX1G) ? button1gbSelected : button1gbNotSelected);
-		buttonPanel.add(selectedMem.equals(XMX1300M) ? button1300mbSelected : button1300mbNotSelected);
-		if (is64bit && mem >= 2000) {
-			buttonPanel.add(selectedMem.equals(XMX2G) ? button2gbSelected : button2gbNotSelected);
+		for (MemoryOption size : MemoryOption.values()) {
+			/*
+			 * Only add button if available.
+			 */
+			if (size.isAvailable()) {
+				/*
+				 * Use selected button if selected, not-selected button otherwise.
+				 */
+				buttonPanel.add(size.getButton(selectedMem == size));
+			}
 		}
-		if (is64bit && mem >= 4000) {
-			buttonPanel.add(selectedMem.equals(XMX4G) ? button4gbSelected : button4gbNotSelected);
-		}
-		if (is64bit && mem >= 8000) {
-			buttonPanel.add(selectedMem.equals(XMX8G) ? button8gbSelected : button8gbNotSelected);
-		}
-		if (is64bit && mem >= 16000) {
-			buttonPanel.add(selectedMem.equals(XMX16G) ? button16gbSelected : button16gbNotSelected);
-		}
+		/*
+		 * Refresh with new button panel.
+		 */
 		removeAll();
 		add(new JLabel("Select the amount of memory ProM may use:"), BorderLayout.NORTH);
 		add(buttonPanel, BorderLayout.CENTER);
 		revalidate();
 	}
 
+	/*
+	 * Updates the necessary files with the new memory option.
+	 */
 	private void updateFiles() {
 		if (oldSelectedMem == selectedMem) {
 			return;
 		}
 		if (OsUtil.isRunningWindows()) {
+			/*
+			 * Windows. Need to update ini file and bat file.
+			 */
 			if (!updateIniFile() || !updateBatFile()) {
-				JOptionPane.showMessageDialog(null, "Unable to set memory limit (-Xmx"  + selectedMem + ") in ProM.l4j.ini and/or ProM641.bat file.\nPlease run the Package Manager as administrator, or set the memory limit manually.");
+				/*
+				 * Something failed. Possibly, the PM was not run in Administrator mode.
+				 */
+				JOptionPane
+						.showMessageDialog(
+								null,
+								"Unable to set memory limit (-Xmx"
+										+ selectedMem
+										+ ") in ProM.l4j.ini and/or ProM641.bat file.\nPlease run the Package Manager as administrator, or set the memory limit manually.");
 				selectedMem = oldSelectedMem;
 				return;
 			}
 		} else if (OsUtil.isRunningLinux() || OsUtil.isRunningUnix()) {
+			/*
+			 * Linux or UNIX. Need to update sh file (and init file for reading default option next time).
+			 */
 			if (!updateIniFile() || !updateShFile()) {
-				JOptionPane.showMessageDialog(null, "Unable to set memory limit (-Xmx"  + selectedMem + ") in ProM.l4j.ini and/or ProM641.sh file.\nPlease set the memory limit manually.");
+				JOptionPane.showMessageDialog(null, "Unable to set memory limit (-Xmx" + selectedMem
+						+ ") in ProM.l4j.ini and/or ProM641.sh file.\nPlease set the memory limit manually.");
 				selectedMem = oldSelectedMem;
 				return;
 			}
 		}
+		/*
+		 * No MAC support (yet), sorry.
+		 */
 		oldSelectedMem = selectedMem;
 	}
-	
+
 	private boolean updateIniFile() {
 		PrintWriter writer;
 		try {
@@ -206,12 +284,14 @@ public class PMMemoryView extends RoundedPanel {
 			return false;
 		}
 	}
-	
+
 	private boolean updateBatFile() {
 		PrintWriter writer;
 		try {
 			writer = new PrintWriter("ProM641.bat", "UTF-8");
-			writer.println("java -ea -Xmx" + selectedMem + " -XX:MaxPermSize=256m -classpath ProM641.jar -Djava.util.Arrays.useLegacyMergeSort=true org.processmining.contexts.uitopia.UI");
+			writer.println("java -da -Xmx"
+					+ selectedMem
+					+ " -XX:MaxPermSize=256m -classpath ProM641.jar -Djava.util.Arrays.useLegacyMergeSort=true org.processmining.contexts.uitopia.UI");
 			writer.close();
 			return true;
 		} catch (FileNotFoundException e) {
@@ -220,7 +300,7 @@ public class PMMemoryView extends RoundedPanel {
 			return false;
 		}
 	}
-	
+
 	private boolean updateShFile() {
 		PrintWriter writer;
 		try {
@@ -235,15 +315,14 @@ public class PMMemoryView extends RoundedPanel {
 			writer.println("do");
 			writer.println("\tadd $lib");
 			writer.println("done");
-			writer.println("java -classpath ${CP} -Djava.library.path=./lib -ea -Xmx${MEM} -XX:MaxPermSize=256m -XX:+UseCompressedOops -Djava.util.Arrays.useLegacyMergeSort=true org.processmining.context.uitopia.UI");
+			writer.println("java -classpath ${CP} -Djava.library.path=./lib -da -Xmx${MEM} -XX:MaxPermSize=256m -XX:+UseCompressedOops -Djava.util.Arrays.useLegacyMergeSort=true org.processmining.context.uitopia.UI");
 			writer.close();
 			return true;
 		} catch (FileNotFoundException e) {
 			return false;
 		} catch (UnsupportedEncodingException e) {
 			return false;
-		}		
+		}
 	}
-	
-	
+
 }
